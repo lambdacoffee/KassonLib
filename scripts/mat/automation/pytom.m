6498637fc7563b957834d5fa0d270b8922b756b2
lambdacoffee <58952805+lambdacoffee@users.noreply.github.com>
2022-11-06 17:08:51 -0500

    Add files via upload

--- scripts/mat/automation/pytom.m
+++ scripts/mat/automation/pytom.m
@@ -1,4 +1,5 @@
-function pytom(src_par_dir)
+function pytom(vars)
+    src_par_dir = vars.SaveParentFolder;
     src_txt_files_dir = fullfile(src_par_dir, 'TraceAnalysis', 'FusionOutput');
     dir_struct = dir(src_txt_files_dir);
     file_arr = {dir_struct(3:end).name};
@@ -7,45 +8,53 @@ function pytom(src_par_dir)
         mkdir(dst_dir);
     end
     
-    mode = fileread('modality.txt');
-    mode = str2double(mode);
+    mode = vars.Mode;
     
     for i=1:length(file_arr)
         filename = char(file_arr{i});
         src_txt_filepath = fullfile(src_txt_files_dir, filename);
         fid = fopen(src_txt_filepath, 'r');
         txt = textscan(fid, '%s');
-        src_mat_filepath = txt{1}{1};
+        src_mat_filename = txt{1}{1};
+        src_mat_filepath = fullfile(src_par_dir, 'TraceAnalysis', src_mat_filename);
         num_traces = length(txt{1}) - 1;
         ind = strfind(filename, 'FusionOutput');
         dst_filename = [filename(1:ind-1), 'pyReviewed.mat'];
         dst_filepath = fullfile(dst_dir, dst_filename);
         data_struct = load(src_mat_filepath);
         dat = data_struct.DataToSave;
-        if mode
-            time_interval = dat.OtherDataToSave.Options.TimeInterval;
-        end
         for j=2:num_traces+1
             line = txt{1}{j};
             line_cell_arr = strsplit(line, ',');
             trace_num = str2double(line_cell_arr{1,1});
             status = str2double(line_cell_arr{1,2});
             isFusion = str2double(line_cell_arr{1,3});
-            fusion_start = str2double(line_cell_arr{1,4});
-            fusion_end = str2double(line_cell_arr{1,5});
-            if length(line_cell_arr) > 5
-                isExclusion = str2double(line_cell_arr{1,6});
+            if mode == 1 || mode == 2
+                time_interval = dat.CombinedAnalyzedTraceData(trace_num).TimeInterval;
             end
+            if mode == 1
+                fusion_start = str2double(line_cell_arr{1,4});
+                fusion_end = str2double(line_cell_arr{1,5});
+                if length(line_cell_arr) > 5
+                    isExclusion = str2double(line_cell_arr{1,6});
+                end
+            elseif mode == 2
+                binding = str2double(line_cell_arr{1,4});
+                fusion_start = str2double(line_cell_arr{1,5});
+                fusion_end = str2double(line_cell_arr{1,6});
+                isExclusion = str2double(line_cell_arr{1,7});
+            end
+            
             curr_trace = dat.CombinedAnalyzedTraceData(trace_num);
             pHDrop = curr_trace.PHdropFrameNum;
             curr_trace.FusionData = struct();
             if status
                 curr_trace.ChangedByUser = 'Reviewed By User';
             end
-            if ~mode
+            if mode == 0
                 time_interval = curr_trace.TimeInterval;
             end
-            if isFusion
+            if isFusion && mode ~= 2
                 % median_fusion_time = median(fusion_start, fusion_end);
                 curr_trace.Designation = 'Fuse';
                 curr_trace.FusionData.Designation = "1 Fuse";
@@ -54,6 +63,17 @@ function pytom(src_par_dir)
                     (fusion_end - fusion_start) * time_interval;
                 curr_trace.FusionData.pHtoFusionTime = ...
                     (fusion_start - pHDrop) * time_interval;
+            elseif isFusion && mode == 2
+                % median_fusion_time = median(fusion_start, fusion_end);
+                curr_trace.Designation = 'Fuse';
+                curr_trace.FusionData.Designation = "1 Fuse";
+                curr_trace.FusionData.FuseFrameNumbers = fusion_end;
+                curr_trace.FusionData.FusionInterval = ...
+                    (fusion_end - fusion_start) * time_interval;
+                curr_trace.FusionData.pHtoFusionTime = ...
+                    binding * time_interval;
+                curr_trace.FusionData.bindingToFusionTime = ...
+                    (fusion_start - binding) * time_interval;
             else
                 curr_trace.Designation = 'No Fusion';
                 curr_trace.FusionData.Designation = 'No Fusion';
